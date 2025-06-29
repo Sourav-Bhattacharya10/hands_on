b8d3554dc675a86661c9e88e71dc9d660057421d
SouravWindows <sourav.bhattacharya3@gmail.com>
2025-06-30 03:55:30 +0530

    Actix web setup 1

--- src/main.rs
+++ src/main.rs
@@ -24,59 +24,78 @@ use to_do::{task_enum::TaskStatus, to_do_factory};
 
 // use common_prefix::find_common_prefix;
 
-fn main() {
-    // let text = "sourav";
-    // println!("Reversed String: {:?}", reverse_string(text));
+// fn main() {
+//     // let text = "sourav";
+//     // println!("Reversed String: {:?}", reverse_string(text));
 
-    // let n: u32 = 5;
-    // println!("Factorial of {} is {}", n, factorial(n));
+//     // let n: u32 = 5;
+//     // println!("Factorial of {} is {}", n, factorial(n));
 
-    // let n: u32 = 7;
-    // println!("Fibonacci of {}th term is {}", n, old_fibonacci(n));
+//     // let n: u32 = 7;
+//     // println!("Fibonacci of {}th term is {}", n, old_fibonacci(n));
 
-    // println!("[ -2, 3, 2, -3 ] → {}", is_balanced_array(vec![ -2, 3, 2, -3 ]));
-    // println!("[ 1, 1, -1, -1 ] → {}", is_balanced_array(vec![ 1, 1, -1, -1 ]));
-    // println!("[ 1, 1, -1 ] → {}", is_balanced_array(vec![ 1, 1, -1 ]));
-    // println!("[ -2, 3, 2, -3, 0, 5, -5 ] → {}", is_balanced_array(vec![ -2, 3, 2, -3, 0, 5, -5 ]));
-    // println!("[ 1, 2, -3 ] → {}", is_balanced_array(vec![ 1, 2, -3 ]));
-    // println!("[ -3, -2, -3, -2, 4, 1, 4, 1, 3, 2, -4, -1 ] → {}", is_balanced_array(vec![ -3, -2, -3, -2, 4, 1, 4, 1, 3, 2, -4, -1 ]));
+//     // println!("[ -2, 3, 2, -3 ] → {}", is_balanced_array(vec![ -2, 3, 2, -3 ]));
+//     // println!("[ 1, 1, -1, -1 ] → {}", is_balanced_array(vec![ 1, 1, -1, -1 ]));
+//     // println!("[ 1, 1, -1 ] → {}", is_balanced_array(vec![ 1, 1, -1 ]));
+//     // println!("[ -2, 3, 2, -3, 0, 5, -5 ] → {}", is_balanced_array(vec![ -2, 3, 2, -3, 0, 5, -5 ]));
+//     // println!("[ 1, 2, -3 ] → {}", is_balanced_array(vec![ 1, 2, -3 ]));
+//     // println!("[ -3, -2, -3, -2, 4, 1, 4, 1, 3, 2, -4, -1 ] → {}", is_balanced_array(vec![ -3, -2, -3, -2, 4, 1, 4, 1, 3, 2, -4, -1 ]));
 
-    // println!("to uppercase : {}", to_uppercase(String::from(text)));
+//     // println!("to uppercase : {}", to_uppercase(String::from(text)));
 
-    // println!(
-    //     "[ 10, 20, 35, 50 ] → {:?}",
-    //     two_pointer_technique_for_two_sum(vec![10, 20, 35, 50], 70i8)
-    // );
-    // println!(
-    //     "[ 10, 20, 30 ] → {:?}",
-    //     two_pointer_technique_for_two_sum(vec![10, 20, 30], 70i8)
-    // );
-    // println!(
-    //     "[ 1, 4, 6, 8, 10, 45 ] → {:?}",
-    //     two_pointer_technique_for_three_sum(vec![1, 4, 6, 8, 10, 45], 13i8)
-    // );
-    // println!(
-    //     "[ 3, 0, 1, 0, 4, 0, 2 ] → {:?}",
-    //     two_pointer_technique_for_rain_water_trap(vec![3, 0, 1, 0, 4, 0, 2])
-    // );
+//     // println!(
+//     //     "[ 10, 20, 35, 50 ] → {:?}",
+//     //     two_pointer_technique_for_two_sum(vec![10, 20, 35, 50], 70i8)
+//     // );
+//     // println!(
+//     //     "[ 10, 20, 30 ] → {:?}",
+//     //     two_pointer_technique_for_two_sum(vec![10, 20, 30], 70i8)
+//     // );
+//     // println!(
+//     //     "[ 1, 4, 6, 8, 10, 45 ] → {:?}",
+//     //     two_pointer_technique_for_three_sum(vec![1, 4, 6, 8, 10, 45], 13i8)
+//     // );
+//     // println!(
+//     //     "[ 3, 0, 1, 0, 4, 0, 2 ] → {:?}",
+//     //     two_pointer_technique_for_rain_water_trap(vec![3, 0, 1, 0, 4, 0, 2])
+//     // );
 
-    // let words_array = vec!["flower", "flow", "flood"];
-    // println!("Common Prefix: {}", find_common_prefix(words_array));
+//     // let words_array = vec!["flower", "flow", "flood"];
+//     // println!("Common Prefix: {}", find_common_prefix(words_array));
 
-    let args: Vec<String> = env::args().collect();
-    let command: &String = &args[1];
-    let title: &String = &args[2];
-    let file_name = "./state.json";
-    let state: Map<String, Value> = read_file(file_name);
-    let status: String;
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            status = "pending".to_owned();
-        }
-    }
-    let item = to_do_factory(title, TaskStatus::from_string(status));
-    process_input(item, command.to_string(), &state, file_name);
+//     let args: Vec<String> = env::args().collect();
+//     let command: &String = &args[1];
+//     let title: &String = &args[2];
+//     let file_name = "./state.json";
+//     let state: Map<String, Value> = read_file(file_name);
+//     let status: String;
+//     match &state.get(*&title) {
+//         Some(result) => {
+//             status = result.to_string().replace('\"', "");
+//         }
+//         None => {
+//             status = "pending".to_owned();
+//         }
+//     }
+//     let item = to_do_factory(title, TaskStatus::from_string(status));
+//     process_input(item, command.to_string(), &state, file_name);
+// }
+
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
